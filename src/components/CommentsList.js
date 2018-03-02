4227f1b0a0c681dcbfddd5ff4b3ee57613d9e7c4
Ad Kru <ad.kru13@gmail.com>
2018-03-02 18:27:10 +0200

    edit form in comments list implemented

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -4,6 +4,7 @@ export default class CommentsList extends Component {
 
 	render() {
 		const commentsArr = this.props.commentsArr || []
+		const self = this
 		const commentsTemplate = commentsArr.map(function (item) {
 			return (
 				<div key={item.id} className='comment'>
@@ -12,16 +13,35 @@ export default class CommentsList extends Component {
 						<p className='comment__author'>Автор: <span className='name'>Ad</span></p>
 					</div>
 					<div className='comment__content'>
+						<button
+							className='toggle-form'
+							onClick={self.toggleForm}
+							ref='toggle-form'>
+						</button>
 						<p className='comment__text'>{item.text}</p>
+						<form  className='comment__form'>
+							<textarea
+								className='edit-comment'
+								defaultValue=''
+								placeholder='Your version'
+								ref='edit-comment'
+							></textarea>
+						</form>
 					</div>
 				</div>
 			)
 		})
+
 		return (
 			<div className='comments-list'>
 				{commentsTemplate}
 			</div>
 		)
+	}
+
+	
+	toggleForm() {
+		console.log('asas');
 	}
 
 }
